d3de72cd8052bd87bff623c7bf5915ca60a5e7db
chxuri <rmamedova2010@gmail.com>
2026-08-29 14:16:11 -0500

    finish drink and snack

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const wakeup = localStorage.getItem('selectedWakeUp');
+const snack = localStorage.getItem('selectedSnack');
 
 document.getElementById('wake-up').textContent = wakeup;
+document.getElementById('snack').textContent = snack;
